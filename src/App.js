cca2afbe3ca2f262b2e1afffb6d0d0e17fbeba89
Nacho Torrella <nachotorrella@gmail.com>
2017-12-29 17:01:20 +0100

    show all pictures  db for all users

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import firebase from "firebase";
 import "./App.css";
-import FileUpload from "./FileUpload"
+import FileUpload from "./FileUpload";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: null
+      user: null,
+      pictures: []
     };
   }
 
@@ -15,6 +16,14 @@ class App extends Component {
     firebase.auth().onAuthStateChanged(user => {
       this.setState({ user });
     });
+    firebase
+      .database()
+      .ref("pictures")
+      .on("child_added", snapshot => {
+        this.setState({
+          pictures: this.state.pictures.concat(snapshot.val())
+        });
+      });
   };
 
   handleAuth = () => {
@@ -25,6 +34,7 @@ class App extends Component {
       .then(res => console.log(`Logged as ${res.user.email}`))
       .catch(err => console.log(`Error ${err.code}: ${err.message}`));
   };
+
   hanndleLogout = () => {
     firebase
       .auth()
@@ -33,14 +43,55 @@ class App extends Component {
       .catch(err => console.log(`Error ${err.code}: ${err.message}`));
   };
 
+  handleUpload = e => {
+    const file = e.target.files[0];
+    const storageRef = firebase.storage().ref(`/photos/${file.name}`);
+    const task = storageRef.put(file);
+    task.on(
+      "state_changed",
+      snapshot => {
+        let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        this.setState({
+          uploadValue: percentage
+        });
+      },
+      err => {
+        console.log(err.message);
+      },
+      () => {
+        const record = {
+          photoURL: this.state.user.photoURL,
+          displayName: this.state.user.displayName,
+          image: task.snapshot.downloadURL
+        };
+        const dbRef = firebase.database().ref("pictures");
+        const newPicture = dbRef.push();
+        newPicture.set(record);
+      }
+    );
+  };
+
   renderLoginButton = () => {
     if (this.state.user) {
       return (
         <div>
-          <img width="100" src={this.state.user.photoURL} alt={this.state.user.displayName} />
+          <img
+            width="100"
+            src={this.state.user.photoURL}
+            alt={this.state.user.displayName}
+          />
           <p>Hola {this.state.user.displayName}!</p>
           <button onClick={this.hanndleLogout}>Logout</button>
-          <FileUpload/>
+          <FileUpload onUpload={this.handleUpload} />
+          {this.state.pictures.map(picture => (
+            <div>
+              <img width="320" src={picture.image} />
+              <br />
+              <img width="100" src={picture.photoURL} alt={picture.displayName} />
+              <br />
+              <span>{picture.displayName}</span>
+            </div>
+          ))}
         </div>
       );
     } else {
@@ -48,7 +99,7 @@ class App extends Component {
         <button type="button" onClick={this.handleAuth}>
           Login with Google
         </button>
-      )
+      );
     }
   };
 
--- src/FileUpload.js
+++ src/FileUpload.js
@@ -1,42 +1,19 @@
 import React, { Component } from "react";
-import firebase from "firebase";
 
 class FileUpload extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      uploadValue: 0,
-      picture: null
+      uploadValue: 0
     };
   }
-
-  handleUpload = e => {
-    const file = e.target.files[0];
-    const storageRef = firebase.storage().ref(`/photos/${file.name}`);
-    const task = storageRef.put(file);
-    task.on("state_changed", snapshot => {
-      let percentage = snapshot.bytesTransferred / snapshot.TotalBytes * 100;
-      this.setState({
-        uploadValue: percentage
-      });
-    }, err => {
-      console.log(err.message)
-    }, () => {
-      this.setState({
-        uploadValue: 100,
-        picture: task.snapshot.downloadURL
-      })
-    });
-  };
 
   render() {
     return (
       <div>
         <progress value={this.state.uploadValue} max="100" />
         <br />
-        <input type="file" onChange={this.handleUpload} />
-        <br />
-        <img width="320" src={this.state.picture} alt="" />
+        <input type="file" onChange={this.props.onUpload} />
       </div>
     );
   }
